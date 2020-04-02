c5c0c4129323b79d044baf6b9b80a501ec7135c8
Cristof <cristofmorissette@hotmail.com>
2020-04-02 11:11:48 -0600

    created Todos and Todo component

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,27 @@ export default {
   name: 'App',
   components: {
 
+  },
+  data() {
+    return {
+      Todos: [
+        {
+          id: 1,
+          title: "My first Todo",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "My second Todo",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "My third Todo",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
